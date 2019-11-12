b41b7f17ee8576b8bf8a599b9a53e7ffc5508341
punithsai <rajapunithsai@gmail.com>
2019-11-12 21:31:30 +0530

    my first git code

---
+++ src/test/java/com/qa/test/DemoTest.java
@@ -0,0 +1,34 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void sum()
+	{
+		System.out.println("SUM");
+         int a=10;
+         int b=20;
+         Assert.assertEquals(30, a+b);
+	}
+	@Test
+	public void sub()
+	{
+		System.out.println("SUb");
+         int a=10;
+         int b=20;
+         Assert.assertEquals(10, b-a);
+	}
+	@Test
+	public void mul()
+	{
+		System.out.println("MUL");
+         int a=10;
+         int b=20;
+         Assert.assertEquals(200, a*b);
+	}
+	
+
+}
